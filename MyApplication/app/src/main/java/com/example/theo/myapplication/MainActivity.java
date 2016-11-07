c8b1a7ac0f35381a5029316f2ceadeb42702a6c7
Karib0u <theofoucher95@gmail.com>
2016-11-07 16:08:29 +0100

    Ajout actionbar + toast

--- MyApplication/app/src/main/java/com/example/theo/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/theo/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.theo.myapplication;
 
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MediaPlayer mp = new MediaPlayer();
-        Button b = (Button) findViewById(R.id.button);
+        Button button = (Button)findViewById(R.id.button);
 
+        // Show toast message when button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"HODOOOOOOOR",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu1, menu);
+        return true;
     }
 }
