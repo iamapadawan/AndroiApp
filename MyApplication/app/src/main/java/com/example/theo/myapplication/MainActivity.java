aef2a87e1007a1686ba8fd2982935e86978f3f11
Karib0u <theofoucher95@gmail.com>
2016-11-08 11:39:59 +0100

    Ajout activité

--- MyApplication/app/src/main/java/com/example/theo/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/theo/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.theo.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -24,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"HODOOOOOOOR",Toast.LENGTH_LONG).show();
             }
         });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+
+        // Show toast message when button is clicked
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("REPONSE");
+                alertDialog.setMessage("LA SAUCISSE !!!");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+    }
+
+    public void sendCommand(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
     @Override
